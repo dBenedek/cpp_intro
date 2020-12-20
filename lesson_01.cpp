20674622a2465dfe3a40ade247805d5ac11c00c0
Benedek Dankó <47271010+dBenedek@users.noreply.github.com>
2020-12-20 14:32:31 +0100

    Add files via upload

---
+++ lesson_01.cpp
@@ -0,0 +1,20 @@
+/*
+ * lesson_01.cpp
+ *  Intro
+ *  Created on: 2020. dec. 20.
+ *      Author: dbenedek
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	std::string name;
+	cout << "Please, enter your first name:" << endl;
+	cin >> name; // console input
+	cout << "Your name: " << name << endl;
+	return 0;
+}
